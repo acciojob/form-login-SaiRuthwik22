38a757ff303779b74c7b32d3717bfeb2f5ae10f9
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-06-07 15:37:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let first = document.getElementById("fname").value
+	let second = document.getElementById("lname").value
+	alert(`${first} ${second}`)
 }
